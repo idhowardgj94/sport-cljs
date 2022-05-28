e5c6e73343d1e5a61e5391f3cf82bf8cefeef83f
howard chang <idhowardgj94@gmail.com>
2022-05-28 11:43:47 +0800

    firebase create user

--- src/main/sports/firebase.cljs
+++ src/main/sports/firebase.cljs
@@ -1,29 +1,45 @@
 (ns sports.firebase
   (:require
    ["regenerator-runtime/runtime"]
-   ["firebase/auth" :as auth :refer [getAuth connectAuthEmulator]]
+   ["firebase/auth" :as auth :refer [getAuth connectAuthEmulator createUserWithEmailAndPassword]]
    ["firebase/app" :as firebase-app :refer [initializeApp]]))
+  ;;  [cljs.core.async :refer [go]]
+  ;;  [cljs.core.async.interop :refer-macros [<p!]]))
 
+;; TODO: manage firebase-config
 (def firebase-config
-  {:apiKey "AIzaSyBPARs63a-_ed_Kl6M61H1b-c_YILiKJEc"
-   :authDoomain "sport-app-dadcc.firebaseapp.com"
-   :projectId "sport-app-dadcc"
-   :storageBucket "sport-app-dadcc.appspot.com"
-   :messagingSenderId "995715164039"
-   :appId "1:995715164039:web:e84afd59557f1076271522"
-   :measurementId "G-4FNN9PWE6R"})
+  {})
 
 ;; :apiKey "AIzaSyBPARs63a-_ed_Kl6M61H1b-c_YILiKJEc"
 (defonce app (atom nil))
 
-(defn init-app
+(defn get-auth
   []
-  (reset! app (initializeApp (clj->js firebase-config))))
-(clj->js firebase-config)
+  (getAuth @app))
+
+(defn init-app
+  ([]
+   (print "1")
+   (reset! app (initializeApp (clj->js firebase-config)))
+   (print "2")
+   (let [auth (get-auth)]
+     (connectAuthEmulator auth "http://localhost:9099")))
+  ([firebase-config]
+   (reset! app (initializeApp (clj->js firebase-config)))))
+
+(defn connect-auth?
+  []
+  (not= @app nil))
+
+(defn create-user
+  "create user with email and password"
+  [email password]
+  (if (connect-auth?)
+    (-> (createUserWithEmailAndPassword (get-auth) email password)
+        (.then #(do (print "then")
+                    (js/console.log (.-user %))))
+        (.catch #(do (print "catch")
+                     (print "error happened!" (.-code %) (.-message %)))))
+    (js/Error. "Didn't connect to firebase service.")))
 (init-app)
-(getAuth @app)
-
-
-
-
-(connectAuthEmulator auth "http://localhost:9099")
+(create-user "howard2@gmail.com" "123456")
