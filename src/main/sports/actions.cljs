5ce83741d28c0d6f074f49bb750ee05ce8a7e576
howard chang <idhowardgj94@gmail.com>
2022-11-23 00:31:05 +0800

    feat: use promise all to get chart.

--- src/main/sports/actions.cljs
+++ src/main/sports/actions.cljs
@@ -4,17 +4,36 @@
    [cljs.core.async.interop :refer-macros [<p!]]
    [sports.firebase.chart :refer [get-exercise-by-startdate-and-enddate]]
    [sports.state :refer [store]]
-   ["date-fns" :refer [endOfToday]]))
+   ["date-fns" :refer [endOfToday addMonths startOfYear endOfYear startOfToday endOfToday]]))
 
 ;; chart_page actions
-(defn get-monthly-duration
+(defn get-monthly-duration!
   "Aim to get monthly duration, start from today"
   []
   (let [start (endOfToday)]
     (.setDate start (- (.getDate start) 31))
-    (swap! store #(do (assoc % :chart/end-date (endOfToday)
-                             assoc % :chart/start-date start)
-                      ))))
+    (swap! store assoc
+           :chart/end-date (endOfToday)
+           :chart/start-date start)))
+
+(defn get-season-duration!
+  "get season duration. a season is three month"
+  []
+  (let [end (endOfToday)
+        start (addMonths end -3)]
+    (swap! store assoc
+           :chart/end-date end
+           :chart/start-date start)))
+
+(defn get-year-duration!
+  "get year duration."
+  []
+  (let [today ()
+        end (endOfYear (endOfToday))
+        start (startOfYear (startOfToday))]
+    (swap! store assoc
+           :chart/end-date end
+           :chart/start-date start)))
 
 (defn set-exercise-group!
   [groups]
@@ -29,16 +48,15 @@
   (swap! store #(assoc % :chart/data []))
   (go
     (try
-      (doseq [it exercises]
-        ;; TODO: use Promise.all
-        (let [data (<p! (get-exercise-by-startdate-and-enddate
-                         uid (:id it) startdate enddate))]
-          ;; if length not 0, swap to store.
-          (if-not (= (.-length (clj->js data)) 0)
-            (swap! store (fn [state] (update state :chart/data
-                                             #(conj % {:data data :name (:name it)})))))))
-      (swap! store #(assoc % :chart/state "done"))
+      (-> (->> exercises
+                (map #(-> (get-exercise-by-startdate-and-enddate uid (:id %) startdate enddate)
+                          (.then (fn [data]
+                                   (if-not (= (.-length (clj->js data)) 0)
+                                     (swap! store (fn [state] (update state :chart/data
+                                                                      (fn [it] (conj it {:data data :name (:name %)}))))))))))
+                (js/Promise.all))
+          (.then #(swap! store assoc :chart/state "done")))
       (catch js/Error e (js/console.log (ex-cause e))))))
-@store
+
 ;;(get-chart-data-by-group!
 ;; "BC7Eplqe9NftRiirrjakIvHMvRx2" test-data (js/Date. "2022-08-01") (js/Date. "2022-09-01"))
