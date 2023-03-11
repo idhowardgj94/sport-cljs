54825372ae9cce8b3719922cf4ac445253b2cb79
howard chang <idhowardgj94@gmail.com>
2023-03-12 00:55:06 +0800

    add re-frame/10x

--- src/main/sports/events.cljs
+++ src/main/sports/events.cljs
@@ -1,5 +1,7 @@
 (ns sports.events
-  (:require [sports.state :refer [store]]))
+  (:require [sports.state :refer [store]]
+            [re-frame.core :as re-frame]
+            ))
 ;; refactor: event out
 (defn login-event!
   [u]
@@ -8,4 +10,21 @@
   (swap! store assoc :user (.-user u))
   (swap! store assoc :auth? true))
 
-
+(re-frame/reg-event-db
+ ::initialize-db
+ (fn [_ _]
+   {:auth? "loading"
+    ;; this is route store.
+    :match nil
+    :validate-msg nil
+    :app nil
+    :user nil
+    :index-db/db nil
+    :exercise/loading "loading"
+    :exercise/groups []
+    :exercise/choose-date {:show false :date (js/Date.)}
+    :chart/start-date nil
+    :chart/end-date nil
+    :chart/state "init"
+    :chart/err-msg nil
+    :chart/data []}))
